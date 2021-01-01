bba80c4923df98c7f7793e9f42e96f4c669d29fa
Jawad Ahmed Khan <studboii@users.noreply.github.com>
2021-01-01 19:53:10 +0530

    Crappify.py updated for noise. Crappify.py now adds randomized compression artifacts and noise in the form of numbers in random spots to an image. Function is also multi-threaded now. I will, if need be, figure out how to run this on a GPU.

--- Crappify.py
+++ Crappify.py
@@ -1,39 +1,62 @@
 import argparse #to pass location
-import cv2 as cv
-import glob #to open folder
+from threading import Thread
+from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
+import random
 
-# TODO: Add noise and compression artifacts to images apart from lower resolution
 
 ap = argparse.ArgumentParser(description="Creat low-res copies of our data and add random noise to it") #Argument object
 ap.add_argument("-l","--location", required = True, help="Path to data-set Folder")
 
 args = vars(ap.parse_args())
+# TODO: Run On Crappy() on GPU using FastAI's Parallel
+def RSzer(im, ScaleVal):
+    w,h = im.size
+    return int(w*ScaleVal),int(h*ScaleVal)
 
-def DownScaler(OgImg, ScaleFactor,FName, Folder):
-	width = int(OgImg.shape[1] * ScaleFactor)
-	height = int(OgImg.shape[0] * ScaleFactor)
-	dim = (width,height) 
-	im = cv.resize(img, dim, interpolation = cv.INTER_AREA)
-	cv.imwrite(Folder+FName,im)
+class Crappifier():
+	"""This class is responsible for resizing our image,adding compression artifacts and some randomly generated noise on top of that.
+	We Pass the High-Res Path, Low-Res Folder and a scale factor that decides by how much we reduce image size"""
+	def __init__(self,HresPath,LresPath,ScaleFactor):
+		self.HresPath = HresPath
+		self.LresPath = LresPath
+		self.ScaleFactor = ScaleFactor
+	def __call__(self,Fname,ShowPaths=False):
+		Savepath = self.LresPath/Fname.relative_to(HiPath) #We do this because there's a way to parallely call the object on multiple files in FastAI which we can save for later
+		ImgObj = Image.open(Fname)#open image
+		TSze = RSzer(ImgObj,self.ScaleFactor)
+		WasaImg = ImgObj.resize(TSze, resample=Image.BILINEAR).convert('RGB')
+		q = random.randint(0,99)
+		if (random.randint(1,2) == 2):
+			ColorVals = (random.randint(0,255),random.randint(0,255),random.randint(0,255)) #Random RGB Values
+			ImageDraw.Draw(WasaImg).text((random.randint(0,TSze[0]//2),random.randint(0,TSze[1]//2)), str(q), fill=ColorVals)
+		WasaImg.save(Savepath,quality=q)
+		if(ShowPaths):
+			print(Savepath)
 
 
-# count = 0
-# TODO: Make Multi-Threaded (?)
-St = len(args["location"])+7
-HalfLoc = args["location"]+"\\Low-Res\\OneHalf"
-FourthLoc = args["location"]+"\\Low-Res\\OneFourth"
-EighthLoc = args["location"]+"\\Low-Res\\OneEighth"
-SixteenthLoc = args["location"]+"\\Low-Res\\OneSixteenth"
-for imagePath in glob.glob(args["location"]+"\\Hi-Res\\*.*"):
-	img = cv.imread(imagePath,cv.IMREAD_UNCHANGED)
-	Fnm = imagePath[St:]
-	DownScaler(img,0.5,Fnm,HalfLoc)
-	DownScaler(img,0.25,Fnm,FourthLoc)
-	DownScaler(img,0.125,Fnm,EighthLoc)
-	DownScaler(img,0.0625,Fnm,SixteenthLoc)	
-	# if count >=3:
-	# 	break
-	# count += 1
+Pth = Path(args["location"]) 
+HiPath = Pth/'Hi-Res'
+LoPath = Pth/'Low-Res'
+
+
+def MakeCrappy(LoPath,ScleF):
+	Crappy = Crappifier(HiPath,LoPath,ScleF)
+	for pt in HiPath.glob("*.*"):
+		Crappy(pt, ShowPaths=True) #Comment this to stop seeing file names as it's done and uncomment the next line
+		# Crappy(pt)
+
+Pths = [LoPath/'OneHalf',LoPath/'OneFourth',LoPath/'OneEighth', LoPath/'OneSixteenth']
+Scale = 0.5
+MakeCrappyThrdLst = []
+for P in Pths:
+	CurThread = Thread(target=MakeCrappy,args=(P,Scale,))
+	MakeCrappyThrdLst.append(CurThread)
+	CurThread.start()
+	Scale = Scale/2
+[Thrd.join() for Thrd in MakeCrappyThrdLst]
+print("Done!!")
+		
 
 
 
